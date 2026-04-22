4796e463fe9943294cec89f2e727cc7afb175cb1
Aanal Patel <aanal.patel@simformsolutions.com>
2026-04-22 12:37:05 +0530

    added max_length in gender field in models

--- Users/models.py
+++ Users/models.py
@@ -21,7 +21,7 @@ class Specialization(models.Model):
     type=models.CharField(choices=Specializations.choices, unique=True,max_length=30)
 
     def __str__(self):
-        return f"{self.type}" 
+        return self.get_type_display()
     
 class Doctor(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE, related_name='doctor_profile')
@@ -30,7 +30,7 @@ class Doctor(models.Model):
     years_of_experience=models.IntegerField()
     is_verified=models.BooleanField(default=False)
     phonenumber=PhoneNumberField(max_length=10)
-    gender=models.CharField(choices=Gender.choices)
+    gender=models.CharField(choices=Gender.choices,max_length=6)
     
 
 class Patient(models.Model):
@@ -38,9 +38,9 @@ class Patient(models.Model):
     user=models.OneToOneField(User,on_delete=models.CASCADE,related_name='patient_profile')
     name=models.CharField(max_length=30)
     age=models.IntegerField()
-    bloodgroup=models.CharField(max_length=2)
-    gender=models.CharField(choices=Gender.choices, max_length=30)
+    bloodgroup=models.CharField(max_length=3)
+    gender=models.CharField(choices=Gender.choices, max_length=6)
     phonenumber=PhoneNumberField(max_length=10)
-    height=models.DecimalField(decimal_places=2,max_digits=4)
-    weight=models.DecimalField(decimal_places=2,max_digits=4)
+    height=models.DecimalField(decimal_places=2,max_digits=6)
+    weight=models.DecimalField(decimal_places=2,max_digits=6)
     city=models.CharField(max_length=30)
