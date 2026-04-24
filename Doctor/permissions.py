8134f6cfe246ee0a804f778e0096a8ac05f61863
Aanal Patel <aanal.patel@simformsolutions.com>
2026-04-24 18:37:44 +0530

    added is_verified permssion in doctor permissiosn

--- Doctor/permissions.py
+++ Doctor/permissions.py
@@ -7,4 +7,4 @@ class IsDoctorUser(BasePermission):
 
     def has_permission(self, request, view):
     
-        return bool(request.user and request.user.is_authenticated and request.user.role=='DOCTOR')
+        return bool(request.user and request.user.is_authenticated and request.user.role=='DOCTOR' and request.user.doctor_profile.is_verified==True)
