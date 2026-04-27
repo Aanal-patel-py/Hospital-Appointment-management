c4e840133c5d34e99489f785cb7dc5c8f50add77
i-aana <aanal.s.patel1705@gmail.com>
2026-04-27 07:53:31 +0530

    added / after doctor/list url

--- Patient/urls.py
+++ Patient/urls.py
@@ -7,7 +7,7 @@ from django.urls import path, include
 
 urlpatterns = [
 
-    path('doctor/list',DoctorListAPIView().as_view()),
+    path('doctor/list/',DoctorListAPIView().as_view()),
     path("doctor-slots/<int:doctor_id>/",SlotAPIView.as_view()),
 
 ]
