04f0484160c84e9ad34daee51998e076a40a54e6
i-aana <aanal.s.patel1705@gmail.com>
2026-04-27 09:04:34 +0530

    changed path for apointment booking

--- Appointments/urls.py
+++ Appointments/urls.py
@@ -8,7 +8,7 @@ from django.urls import path, include
 
 urlpatterns = [
 
-    path("slots/<int:slot_id>/book/",BookSlotAPIView.as_view()),
+    path("appointments/<int:slot_id>/book/",BookSlotAPIView.as_view()),
     path("appointments/<int:appointment_id>/confirm/",ConfirmAppointmentAPIView.as_view()),
     path("appointments/<int:appointment_id>/reject/",RejectAppointmentAPIView.as_view()),
 ]
