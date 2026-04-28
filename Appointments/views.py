e6ef4f620145c001cb4ecc36236487e70f4bb4db
Aanal Patel <aanal.patel@simformsolutions.com>
2026-04-28 12:43:54 +0530

    cusstomized the email properly

--- Appointments/views.py
+++ Appointments/views.py
@@ -35,19 +35,27 @@ class BookSlotAPIView(APIView):
 
     def patch(self,request,slot_id):
         patient=request.user.patient_profile
+      
 
         try:
             with transaction.atomic():
                 slot=slot_availability.objects.select_for_update().select_related("schedule","schedule__doctor").get(id=slot_id)
+                print(f"{slot_id}")
                 if slot.is_booked:
                     return Response({"error":"Slot already booked"},status=400)
                 slot.is_booked=True
                 slot.save()
 
                 appointment=Appointment.objects.create(patient=patient,doctor=slot.schedule.doctor,slot=slot,status=AppointmentStatus.PENDING)
-                # send_email_task.delay('Appointment sent for Confirmation','your appointment has been sent to the doctor please wait for it to be confirmed',['aanal.patel@simformsolutions.com'])
+                message_body=f"""Appointment request for 
+                date:{appointment.slot.date}, 
+                slot:{appointment.slot.start_time} to {appointment.slot.end_time} 
+                patient: {appointment.patient.name}"""
+                email=appointment.doctor.user.email
 
-                return Response({"message":"Appointment booked successfully","appointment_id":appointment.id},status=201)
+                # send_email_task.delay('Appointment request',message_body,[email])
+
+                return Response({"message":"Appointment request has been sent to the doctor , please wait for confirmation","appointment_id":appointment.id},status=201)
 
         except slot_availability.DoesNotExist:
             return Response({"error":"Slot not found"},status=404)
@@ -61,7 +69,15 @@ class ConfirmAppointmentAPIView(APIView):
 
         appointment.status=AppointmentStatus.CONFIRMED
         appointment.save()
-        # send_email_task.delay('Appointment Confirmed','your appointment has been confirmed for the respective slot you created',['aanal.patel@simformsolutions.com'])
+        message_body=f"""
+        Your Appointment has been Confirmed for 
+        doctor:{appointment.doctor.name},
+        Date:{appointment.slot.date},
+        Slot:{appointment.slot.start_time} to {appointment.slot.end_time} 
+        please be on time , Thankyou.
+        """
+        email=appointment.patient.user.email
+        # send_email_task.delay('Appointment Confirmed',message_body,[email])
 
         return Response(
             {"message":"Appointment confirmed"}
@@ -74,14 +90,22 @@ class RejectAppointmentAPIView(APIView):
         doctor=request.user.doctor_profile
 
         with transaction.atomic():
-            appt=Appointment.objects.select_for_update().get(id=appointment_id,doctor=doctor)
-            appt.status=AppointmentStatus.CANCELLED
-            appt.save()
+            appointment=Appointment.objects.select_for_update().get(id=appointment_id,doctor=doctor)
+            appointment.status=AppointmentStatus.CANCELLED
+            appointment.save()
 
-            appt.slot.is_booked=False
-            appt.slot.save()
-            # send_email_task.delay('Appointment Rejected','your appointment has been Rejected , try another slot timing',['aanal.patel@simformsolutions.com'])
+            appointment.slot.is_booked=False
+            appointment.slot.save()
 
+            message_body=f"""
+            Your Appointment has been rejected for 
+            doctor:{appointment.doctor.name},
+            Date:{appointment.slot.date},
+            Slot:{appointment.slot.start_time} to {appointment.slot.end_time} 
+            We apologize for inconvienence, please try to book another slot, Thankyou.
+            """
+            email=appointment.patient.user.email
+            # send_email_task.delay('Appointment Rejected',message_body,[email])
 
         return Response(
             {"message":"Appointment rejected"}
